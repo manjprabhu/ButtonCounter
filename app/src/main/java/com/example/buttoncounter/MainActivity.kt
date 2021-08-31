7b8e3e4b4946c7e11e75b27c29653e6a5abcd5e6
MPB <mp4learning@gmail.com>
2021-08-31 21:18:19 +0530

    Type checking using "is" operator.

--- app/src/main/java/com/example/buttoncounter/MainActivity.kt
+++ app/src/main/java/com/example/buttoncounter/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.buttoncounter
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.buttoncounter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private var TAG : String =  "MainActivity"
     private lateinit var mBinding: ActivityMainBinding
     var count =0
 
@@ -18,6 +20,27 @@ class MainActivity : AppCompatActivity() {
         mBinding.button.setOnClickListener {
             count += 1
             mBinding.textview.text = count.toString()
+        }
+
+        isOperatorTesting()
+    }
+
+    fun isOperatorTesting() {
+        var name = "hello"
+        var age = 25
+        var salary = 1000.0
+
+        var details: List<Any> =  listOf(name,age,salary)
+
+        for(attribute in details) {
+
+            if(attribute is String) {
+                Log.v(TAG,"Name:" +name)
+            } else if(attribute is Int) {
+                Log.v(TAG,"Age:"+age)
+            } else if(attribute is Double) {
+                Log.v(TAG,"Salary:"+salary)
+            }
         }
     }
 }
