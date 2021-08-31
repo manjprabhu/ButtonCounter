4e98737e232671cbbddbf702573e8a8810bfeac0
MPB <mp4learning@gmail.com>
2021-08-31 20:13:05 +0530

    Removed unused code.

--- app/src/main/java/com/example/buttoncounter/MainActivity.kt
+++ app/src/main/java/com/example/buttoncounter/MainActivity.kt
@@ -8,25 +8,16 @@ import com.example.buttoncounter.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mBinding: ActivityMainBinding
+    var count =0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         mBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
-
-        var count =0
         mBinding.button.setOnClickListener {
             count += 1
             mBinding.textview.text = count.toString()
-
         }
-
-        /* var button = findViewById<Button>(R.id.button)
-         var textview = findViewById<TextView>(R.id.textview)
-         button.setOnClickListener {
-             count += 1
-             textview.text = count.toString()
-         }*/
     }
 }
