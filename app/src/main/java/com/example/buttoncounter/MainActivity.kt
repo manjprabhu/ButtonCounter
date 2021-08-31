b5bc5d9e74adc5a4de2cf40a2be8ba7227fdab79
MPB <mp4learning@gmail.com>
2021-08-31 19:43:12 +0530

    Added Databinding support

--- app/src/main/java/com/example/buttoncounter/MainActivity.kt
+++ app/src/main/java/com/example/buttoncounter/MainActivity.kt
@@ -1,21 +1,32 @@
 package com.example.buttoncounter
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.buttoncounter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var button = findViewById<Button>(R.id.button)
-        var textview = findViewById<TextView>(R.id.textview)
-        var count = 0
-        button.setOnClickListener {
+        mBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        var count =0
+        mBinding.button.setOnClickListener {
             count += 1
-            textview.text = count.toString()
+            mBinding.textview.text = count.toString()
+
         }
+
+        /* var button = findViewById<Button>(R.id.button)
+         var textview = findViewById<TextView>(R.id.textview)
+         button.setOnClickListener {
+             count += 1
+             textview.text = count.toString()
+         }*/
     }
 }
